839f32016bcfa939a06f63bedf76c1e05fb3a35b
Venkata <venkata@TanusMacBook.local>
2016-12-04 19:26:41 -0800

    Simple UI to check dividend details for specific stock

---
+++ financial/ui/dividend_details.js
@@ -0,0 +1,13 @@
+angular.module('demo', [])
+.controller('quote', function($scope, $http) {
+	$scope.getDividendInformation = function()	{
+
+		var get_url = "http://localhost:8081/stockautomation/api/v1.0/getdividenddetails/" + $scope.stock_symbol ;
+           
+    	$http.get(get_url).
+        then(function(response) {
+            $scope.stock_quote = response.data;
+        });
+    };
+});
+
