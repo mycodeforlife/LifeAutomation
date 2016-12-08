759d7cb1512ef6b8ba21bf1b31f5f9f06643933f
Venkata <venkata@TanusMacBook.local>
2016-12-08 00:48:50 -0800

    Moved qutoe object __init__

--- financial/src/StockAnalysis.py
+++ financial/src/StockAnalysis.py
@@ -5,45 +5,42 @@ from yahoo_finance import Share
 class StockAnalysis:
 
 	stock_symbol = 'YHOO'	
+	stock_company_name = "Yahoo Inc,"
+	quote = Share(stock_symbol)
 
 	def __init__(self,symbol):
 		self.stock_symbol = symbol
-	
+		self.quote = Share(self.stock_symbol)
+		
 	def getStockSymbol(self):
 		return self.stock_symbol 
 
 	def getStockPriceInfo(self,symbol):
-		quote = Share(symbol)
-		stock_price = quote.get_price()
-		currency = quote.get_currency()
+		stock_price = self.quote.get_price()
+		currency = self.quote.get_currency()
 		price_info = {'Symbol':symbol,'Price':stock_price,'Currency':currency}
 		return price_info
 
 	def getDividendYieldInfo(self,symbol):
-		quote = Share(symbol)
-		dividend_yield = quote.get_dividend_yield()
+		dividend_yield = self.quote.get_dividend_yield()
 		dividend_info = {'Symbol':symbol,'DividendYield':dividend_yield,'Unit':'%'}
 		return dividend_info
 
 	def getPayOurRatio(self,symbol):
-		quote = Share(symbol)
-		dividend_per_share = quote.get_dividend_share()
-		EPS_current_year = quote.get_EPS_estimate_current_year()
+		dividend_per_share = self.quote.get_dividend_share()
+		EPS_current_year = self.quote.get_EPS_estimate_current_year()
 		pay_out_ratio = (float(dividend_per_share) / float(EPS_current_year))*100
 		pay_out_ratio =  float("{0:.2f}".format(pay_out_ratio))
 		pay_out_info = {'Symbol':symbol,'PayOut Ratio':pay_out_ratio, "Unit":"%" }
 		return pay_out_info
 
 	def getDeividendDetails(self,symbol):
-		quote = Share(symbol)
-		dividend_per_share = quote.get_dividend_share()
-		EPS_current_year = quote.get_EPS_estimate_current_year()
-		dividend_yield = quote.get_dividend_yield()
-		hold_date = quote.get_ex_dividend_date()
-		payout_date = quote.get_dividend_pay_date()
+		dividend_per_share = self.quote.get_dividend_share()
+		EPS_current_year = self.quote.get_EPS_estimate_current_year()
+		dividend_yield = self.quote.get_dividend_yield()
+		hold_date = self.quote.get_ex_dividend_date()
+		payout_date = self.quote.get_dividend_pay_date()
 
-		print dividend_per_share
-		print EPS_current_year
 		if dividend_per_share is None:
 			dividend_per_share = "0"
 			dividend_yield = "0%"
@@ -56,9 +53,8 @@ class StockAnalysis:
 		return dividend_info
 	
 	def getExDividedDate(self,symbol):
-		quote = Share(symbol)
-		hold_date = quote.get_ex_dividend_date()
-		payout_date = quote.get_dividend_pay_date()
+		hold_date = self.quote.get_ex_dividend_date()
+		payout_date = self.quote.get_dividend_pay_date()
 		data = {'Symbol':symbol,'ExDividedDate':hold_date,'LastPayoutDate':payout_date}
 		return data
 		
@@ -68,9 +64,8 @@ class StockAnalysis:
 	def getDividendDateDetails(self):
 		pass
 	def getNumberOfOutStandingShares(self,symbol):
-		quote = Share(symbol)
-		total_market_cap = quote.get_market_cap()
-		current_price = quote.get_prev_close()
+		total_market_cap = self.quote.get_market_cap()
+		current_price = self.quote.get_prev_close()
 		if "B" in total_market_cap:
 			total_market_cap = total_market_cap.rstrip("B")
 			total_market_cap = float(total_market_cap)*1000000000
