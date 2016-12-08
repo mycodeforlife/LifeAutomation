b6e6a7502db32135d009be079d5ade28ff015992
Venkata <venkata@TanusMacBook.local>
2016-12-07 23:13:43 -0800

    Added news Feeds and fixed display in UI

--- financial/src/NewsFeeds.py
+++ financial/src/NewsFeeds.py
@@ -18,13 +18,14 @@ class NewsFeedsFromSeekingAlpha:
 		headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36'}
 		response = requests.get(self.request_url, headers=headers)
 		temp = response.content
-		
+		#print temp
 		# trying to replace special character to fix the Unicode error: UnicodeEncodeError: 'ascii' codec can't encode character u'\u2019' in position 30: ordinal not in range(128)
-
+		temp = temp.replace("\"","'")
 		original_str = "<?xml version=\"1.0\"?>"
 		new_str = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 
 		temp = temp.replace(original_str,new_str)
+		
 		response_data = temp.replace("&#x2019;","")
 		
 		root = ET.fromstring(response_data)
@@ -34,11 +35,13 @@ class NewsFeedsFromSeekingAlpha:
 				news_date = str(i.find("pubDate").text)
 				news_title = str(i.find("title").text)
 				news_url = str(i.find("link").text)
+				#print news_title,news_url,news_date
 				self.news_response_json = self.news_response_json+"{\"title\":\""+news_title+"\",\"url\":\""+news_url+"\",\"publishedDate\":\""+news_date+"\"},"
 
 		self.news_response_json=self.news_response_json.rstrip(",")		
 		self.news_response_json=self.news_response_json+"]"
 
 	def getNewsInJSONFormat(self):
+		#print self.news_response_json
 		return json.loads(self.news_response_json)
 	
